15d10ccd32dd9896a63b3e7969c8d42ecc0dffdb
Barbue <david.crochet@codeimmersives.com>
2022-09-12 18:30:45 -0500

    Update main.js file

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ console.log("HH   HH   eee    lll lll  oooo  ww        ww  oooo  rr rr lll      
 console.log("HHHHHHH ee   e   lll lll oo  oo  ww      ww  oo  oo rrr r lll  dddddd   !!!")
 console.log("HH   HH eeeee    111 111 oo  oo   ww ww ww   oo  oo rr    lll dd   dd")
 console.log("HH   HH  eeeee   111 111  oooo     ww ww      oooo  rr    lll  dddddd  !!!!")
+
+
+
+console.log("____    ____  _______ .______      ____    ____    .___  ___.      ___        _______  __    ______      ___       __  ")
+console.log("\   \  /   / |   ____||   _  \     \   \  /   /    |   \/   |     /   \      /  _____||  |  /      |    /   \     |  | ")   
+console.log(" \   \/   /  |  |__   |  |_)  |     \   \/   /     |  \  /  |    /  ^  \    |  |  __  |  | |  ,----'   /  ^  \    |  | ")
+console.log("  \      /   |   __|  |      /       \_    _/      |  |\/|  |   /  /_\  \   |  | |_ | |  | |  |       /  /_\  \   |  | ")
+console.log("   \    /    |  |____ |  |\  \----.    |  |        |  |  |  |  /  _____  \  |  |__| | |  | |  `----. /  _____  \  |  `----.")
+console.log("    \__/     |_______|| _| `._____|    |__|        |__|  |__| /__/     \__\  \______| |__|  \______|/__/     \__\ |_______|")
+                                                                                                                           
+
+
+
+  
+   
+
