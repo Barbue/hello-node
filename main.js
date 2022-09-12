af820a0ce2b827cd6c0efa50d601f0707ca9d00a
Barbue <david.crochet@codeimmersives.com>
2022-09-12 15:05:59 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Hello World!")
+
+
+console.log("HH   HH          lll lll                                  lll      dd   !!!");
+console.log("HH   HH   eee    lll lll  oooo  ww        ww  oooo  rr rr lll      dd   !!!")
+console.log("HHHHHHH ee   e   lll lll oo  oo  ww      ww  oo  oo rrr r lll  dddddd   !!!")
+console.log("HH   HH eeeee    111 111 oo  oo   ww ww ww   oo  oo rr    lll dd   dd")
+console.log("HH   HH  eeeee   111 111  oooo     ww ww      oooo  rr    lll  dddddd  !!!!")
